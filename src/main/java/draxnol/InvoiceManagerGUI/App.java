f81dc3216a91c7084732b6356e7ae49b9af92a6d
robert <kaczurr@gmail.com>
2020-02-29 17:42:13 -0500

    Created Main Window GUI.

--- src/main/java/draxnol/InvoiceManagerGUI/App.java
+++ src/main/java/draxnol/InvoiceManagerGUI/App.java
@@ -20,6 +20,7 @@ public class App extends Application {
         scene = new Scene(loadFXML("primary"));
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("Invoice Manager");
     }
 
     static void setRoot(String fxml) throws IOException {
